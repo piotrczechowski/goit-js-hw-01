820e385f25a8a169d80b49000f26270dfaf3eca8
Piotr Czechowski <czechowp@gmail.com>
2024-09-29 16:47:52 +0200

    task 2

--- js/task-1.js
+++ js/task-1.js
@@ -8,3 +8,4 @@ function makeTransaction(quantity, pricePerDroid) {
 console.log(makeTransaction(5, 3000));
 console.log(makeTransaction(3, 1000));
 console.log(makeTransaction(10, 500));
+console.log(makeTransaction(100, 10))
---
+++ js/task-2.js
@@ -0,0 +1,8 @@
+function getShippingMessage(country, price, deliveryFee) {
+    let totalPrice = price + deliveryFee;
+    return "Shipping to $ {country} will cost $ {totalPrice} credits "
+}
+
+console.log(getShippingMessage("Australia", 120, 50));
+console.log(getShippingMessage("Germany", 80, 20));
+console.log(getShippingMessage("Sweden", 100, 20));
